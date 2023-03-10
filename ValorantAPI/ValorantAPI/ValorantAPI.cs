432bbc2cfeb72b4c5e23e0d927ac600c17ccebdb
Antony Bragg <antonybragg@gmail.com>
2023-03-10 11:23:47 +0000

    Basics working

--- ValorantAPI/ValorantAPI/ValorantAPI.cs
+++ ValorantAPI/ValorantAPI/ValorantAPI.cs
@@ -1,24 +1,53 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
 using Newtonsoft.Json;
 using ValorantAPI.Models;
 
 namespace ValorantAPI
 {
+    public class AgentsResponse
+    {
+        public List<AgentModel> Data { get; set; }
+    }
+
     public class ValorantAPI
     {
-        public static List<AgentModel> GetAgents()
+        public static List<AgentModel> GetAllAgents()
         {
-            string json;
+            List<AgentModel> agents;
+
             using (var webClient = new System.Net.WebClient())
             {
-                json = webClient.DownloadString("https://valorant-api.com/v1/agents");
+                var json = webClient.DownloadString("https://valorant-api.com/v1/agents");
+                AgentsResponse response = JsonConvert.DeserializeObject<AgentsResponse>(json);
+                agents = response.Data;
             }
-
-            List<AgentModel> data = JsonConvert.DeserializeObject<List<AgentModel>>(json);
-            List<AgentModel> agents = data;
 
             return agents;
         }
+
+        public static AgentModel GetAgentByName(string AgentName)
+        {
+            var agents = GetAllAgents();
+            var agent = agents.Where(a => a.displayName == AgentName).FirstOrDefault();
+
+            return agent;
+        }
+
+        //public static SingleAgent GetAgentByUUID(string AgentUuid)
+        //{
+        //    var url = $"https://valorant-api.com/v1/agents/{AgentUuid}";
+        //    var agent = new SingleAgent();
+        //    using (var webClient = new System.Net.WebClient())
+        //    {
+        //        var json = webClient.DownloadString(url);
+        //        agent = JsonConvert.DeserializeObject<SingleAgent>(json);
+        //    }
+
+        //    return agent;
+        //}
     }
 }
--- ValorantAPI/ValorantAPIConsoleApp/Program.cs
+++ ValorantAPI/ValorantAPIConsoleApp/Program.cs
@@ -1,5 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Valorant API Testing");
+//add6443a-41bd-e414-f6ad-e58d267f4e95
 
+//var test = await ValorantAPI.ValorantAPI.GetAgentAsync("add6443a-41bd-e414-f6ad-e58d267f4e95");
 
-ValorantAPI.ValorantAPI.GetAgents();
+var test = ValorantAPI.ValorantAPI.GetAgentByName("Jett");
+
+Console.WriteLine(test.description);
+
+//var test = ValorantAPI.ValorantAPI.GetAllAgents();
+
+//foreach (var agent in test)
+//{
+//    Console.WriteLine(agent.displayName);
+//    Console.WriteLine(agent.description);
+//    Console.WriteLine(agent.uuid);
+//    foreach (var ability in agent.abilities)
+//    {
+//        Console.WriteLine(ability.displayName);
+//        Console.WriteLine(ability.description);
+//    }
+//    Console.WriteLine("----");
+//}
