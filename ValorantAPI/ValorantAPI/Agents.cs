deb63b4947a6f101a036aa6f16ca54758b47bb44
Antony Bragg <antonybragg@gmail.com>
2023-03-16 17:00:23 +0000

    Cleanup

--- ValorantAPI/ValorantAPI/Agents.cs
+++ ValorantAPI/ValorantAPI/Agents.cs
@@ -1,9 +1,7 @@
 ﻿using System;
-using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using Newtonsoft.Json;
-using Newtonsoft.Json.Linq;
 using ValorantAPI.Models;
 
 namespace ValorantAPI
