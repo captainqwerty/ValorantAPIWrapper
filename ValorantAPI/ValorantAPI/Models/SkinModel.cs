d3e111a4f83a480da6fccf5b135ddaf49b2fcab9
Antony Bragg <antonybragg@gmail.com>
2023-04-19 23:17:28 +0100

    Added Skin Model

---
+++ ValorantAPI/ValorantAPI/Models/SkinModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ValorantAPI.Models
+{
+    internal class SkinModel
+    {
+    }
+}
