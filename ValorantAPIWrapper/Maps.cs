6890aff2c2bc074d48faff221e1db4fadd4ac4ba
Antony Bragg <antonybragg@gmail.com>
2023-05-12 18:18:54 +0100

    Update Maps.cs

--- ValorantAPIWrapper/Maps.cs
+++ ValorantAPIWrapper/Maps.cs
@@ -9,15 +9,17 @@ namespace ValorantAPIWrapper
         /// Retrieves a list of all maps in Valorant from the Valorant API.
         /// </summary>
         /// <returns>A list of MapModel objects representing each map in Valorant.</returns>
-        public static List<MapModel> GetAllMaps()
+        public async static Task<List<MapModel>> GetAllMaps()
         {
             List<MapModel> maps;
 
-            using (var webClient = new System.Net.WebClient())
+            using (var httpClient = new HttpClient())
             {
-                var json = webClient.DownloadString("https://valorant-api.com/v1/maps");
-                MapResponse response = JsonConvert.DeserializeObject<MapResponse>(json);
-                maps = response.Data;
+                var uri = new Uri("https://valorant-api.com/v1/maps");
+                var response = await httpClient.GetAsync(uri);
+                var json = await response.Content.ReadAsStringAsync();
+                var mapResponse = JsonConvert.DeserializeObject<MapResponse>(json);
+                maps = mapResponse.Data;
             }
 
             return maps;
@@ -28,16 +30,16 @@ namespace ValorantAPIWrapper
         /// </summary>
         /// <param name="MapName">The name of the map to retrieve.</param>
         /// <returns>A MapModel object representing the map with the specified name, or null if no map is found.</returns>
-        public static MapModel GetMapByName(string MapName)
+        public async static Task<MapModel> GetMapByName(string MapName)
         {
-            return GetAllMaps().Where(a => a.displayName.Equals(MapName.Trim(), StringComparison.OrdinalIgnoreCase)).FirstOrDefault();
+            return await GetAllMaps().Where(a => a.displayName.Equals(MapName.Trim(), StringComparison.OrdinalIgnoreCase)).FirstOrDefault();
         }
 
         /// <summary>
         /// Retrieves a random map in Valorant from the list of available maps.
         /// </summary>
         /// <returns>A MapModel object representing the randomly selected map.</returns>
-        public static MapModel GetRandomMap()
+        public async static Task<MapModel> GetRandomMap()
         {
             var maps = GetAllMaps();
             Random random = new Random(Guid.NewGuid().GetHashCode()); // Use a unique seed value
