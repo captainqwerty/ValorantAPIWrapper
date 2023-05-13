7a016ed3482a27e7bc79b4c0a46ed2393476fa95
Antony Bragg <antonybragg@gmail.com>
2023-05-13 22:25:51 +0100

    Example program added

---
+++ Example/Program.cs
@@ -0,0 +1,47 @@
+﻿using ValorantAPIWrapper;
+
+Console.WriteLine("Valorant API Wrapper Demo");
+
+// Map Example
+Console.WriteLine("Map Name:");
+string mapName = Console.ReadLine();
+var map = await Maps.GetMapByName(mapName);
+
+Console.WriteLine($"Following callouts");
+
+foreach (var callout in map.callouts)
+{
+    Console.WriteLine(callout.regionName);
+}
+
+// Agent Example
+Console.WriteLine("Agents Name:");
+string agentName = Console.ReadLine();
+var agent = await Agents.GetAgentByName(agentName);
+
+Console.WriteLine($"{agent.displayName} is a {agent.role.displayName} and their developer was {agent.developerName}. {agent.displayName} has the following abilties:");
+Console.WriteLine("");
+foreach (var ability in agent.abilities)
+{
+    Console.WriteLine(ability.displayName);
+    Console.WriteLine(ability.description);
+    Console.WriteLine("");
+}
+Console.WriteLine("");
+
+// Weapon Example
+Console.WriteLine("Weapon Name:");
+string weaponName = Console.ReadLine();
+var weapon = await Weapons.GetWeaponByName(weaponName);
+
+Console.WriteLine($"{weapon.displayName}");
+Console.WriteLine("");
+foreach (var skin in weapon.skins)
+{
+    Console.WriteLine(skin.displayName);
+    foreach (var chroma in skin.chromas)
+    {
+        Console.WriteLine(chroma.displayName);
+    }
+    Console.WriteLine("");
+}
